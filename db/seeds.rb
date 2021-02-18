61bbfccffe8378a1a5a183a1ebde87656aaf1333
Ruben Rubilar <rubenrubilarbustos@gmail.com>
2021-02-18 19:06:41 +0900

    added routes, model, controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = open(url).read
+objs = JSON.parse(ingredients)
+
+puts "cleaning up db"
+Ingredient.destroy_all
+puts "db is clean"
+
+objs["drinks"].each do |key|
+  INGREDIENT = Ingredient.create!(
+    name: "#{key["strIngredient1"]}"
+    )
+  puts "#{INGREDIENT.name} created!"
+end
