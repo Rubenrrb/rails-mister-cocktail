f326684704a9b2a6c56a1583cd70b99698f625c8
Ruben Rubilar <rubenrubilarbustos@gmail.com>
2021-03-18 04:20:41 +0900

    media

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,17 +8,17 @@ class CocktailsController < ApplicationController
       @cocktail = Cocktail.find(params[:id])
       @dose = Dose.new
     end
-    
+
     def new
       @cocktail = Cocktail.new
     end
-    
+
     def create
       @cocktail = Cocktail.new(cocktail_params)
       if @cocktail.save
         redirect_to cocktail_path(@cocktail)
       else
-        render :new
+        render 'form_add_dose'
       end
     end
 
@@ -28,7 +28,7 @@ class CocktailsController < ApplicationController
         redirect_to cocktails_path
 
       end
-      
+
     private
 
     def cocktail_params
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -10,7 +10,7 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to cocktail_path(@corcktail)
     else
       render :new
     end
